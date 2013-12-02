6bb5a8ae5fda59706ec9b9de18d72f770accd7b0
ardroz <darr.0725@gmail.com>
2013-12-02 11:48:37 -0600

    Structure finished

---
+++ app.js
@@ -0,0 +1,47 @@
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.locals.pretty = true;
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+  app.use(require('less-middleware')({ src: __dirname + '/public' }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+app.get('/ba', routes.ba);
+app.get('/ba_historia', routes.ba_historia);
+app.get('/ba_visita', routes.ba_visita);
+app.get('/ba_localizacion', routes.ba_localizacion);
+app.get('/mr', routes.mr);
+app.get('/mr_historia', routes.mr_historia);
+app.get('/mr_visita', routes.mr_visita);
+app.get('/mr_localizacion', routes.mr_localizacion);
+app.get('/tl', routes.tl);
+app.get('/tl_historia', routes.tl_historia);
+app.get('/tl_visita', routes.tl_visita);
+app.get('/tl_localizacion', routes.tl_localizacion);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
---
+++ routes/index.js
@@ -0,0 +1,56 @@
+
+/*
+ * GET home page.
+ */
+
+exports.index = function(req, res){
+  res.render('index', { title: 'Df' });
+};
+
+exports.mr = function(req, res){
+  res.render('mr', { title: 'Df', nav: 'mr' });
+};
+
+exports.mr_historia = function(req, res){
+  res.render('mr_historia', { title: 'Df', nav: 'mr' });
+};
+
+exports.mr_visita = function(req, res){
+  res.render('mr_visita', { title: 'Df', nav: 'mr' });
+};
+
+exports.mr_localizacion = function(req, res){
+  res.render('mr_localizacion', { title: 'Df', nav: 'mr' });
+};
+
+exports.tl = function(req, res){
+  res.render('tl', { title: 'Df', nav: 'tl' });
+};
+
+exports.tl_historia = function(req, res){
+  res.render('tl_historia', { title: 'Df', nav: 'mr' });
+};
+
+exports.tl_visita = function(req, res){
+  res.render('tl_visita', { title: 'Df', nav: 'mr' });
+};
+
+exports.tl_localizacion = function(req, res){
+  res.render('tl_localizacion', { title: 'Df', nav: 'mr' });
+};
+
+exports.ba = function(req, res){
+  res.render('ba', { title: 'Df', nav: 'ba' });
+};
+
+exports.ba_historia = function(req, res){
+  res.render('ba_historia', { title: 'Df', nav: 'mr' });
+};
+
+exports.ba_visita = function(req, res){
+  res.render('ba_visita', { title: 'Df', nav: 'mr' });
+};
+
+exports.ba_localizacion = function(req, res){
+  res.render('ba_localizacion', { title: 'Df', nav: 'mr' });
+};
